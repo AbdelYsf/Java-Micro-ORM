bbe5acc8ec8c7f537a03c9e4c3b529662f4dda20
abdellah <abdel.ysf@gmail.com>
2020-02-01 12:50:37 +0100

    changing database values

--- src/ORM/DataSource.java
+++ src/ORM/DataSource.java
@@ -1,10 +1,10 @@
 package ORM;
 
 public class DataSource {
-	public static final String user="root";
-	public static final String pwd="";
+	public static final String user="user";
+	public static final String pwd="password";
 	public static final String driver="com.mysql.jdbc.Driver";
-	public static final String url="jdbc:mysql://localhost:3306/tpjava";
+	public static final String url="jdbc:mysql://localhost:3306/db_name";
 	
 	
 }
